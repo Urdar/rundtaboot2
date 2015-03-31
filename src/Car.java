10b326df3e9293ed41111960d90a57620e40caae
Urdar <jens.vingen@gmail.com>
2015-03-31 14:03:14 +0200

    La til kommentar på kjøring/logikk

--- src/Car.java
+++ src/Car.java
@@ -2,4 +2,7 @@
  * Created by jens on 31.03.15.
  */
 public class Car {
+    private int speed;
+    private int maxSpeed;
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,14 @@
  */
 public class Main {
 }
+
+/*
+TODO slett dete når logikk er på plass
+http://gamedev.stackexchange.com/questions/9607/moving-an-object-in-a-circular-path
+I want to move one object (dot) in a circular path. How should I change the X and Y coordinates to accomplish this?
+
+You can make it by using simple math:
+X := originX + sin(angle)*Size;
+Y := originY + cos(angle)*Size;
+
+ */
